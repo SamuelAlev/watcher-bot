b53541899e38efabb654f34c6924b04b61248d73
Samuel Alev <samuel.alev@protonmail.com>
2021-05-09 02:02:51 +0200

    chore: fix typo

--- scripts/setup/inject/askBotRoleStep.js
+++ scripts/setup/inject/askBotRoleStep.js
@@ -6,6 +6,6 @@ const onPermissionPopupConfirm = () => {
 window.openModal(
     'Bot permission',
     'https://raw.githubusercontent.com/SamuelAlev/watcher-bot/main/src/asset/bot-permissions-webhook.png',
-    '<b>You can now opne the Discord app if you want or use the web app.</b><br><br>The bot doesn\'t have any permissions yet, please create a role where the bot can "Manage webhooks" and assign it to the bot.',
+    '<b>You can now open the Discord app if you want or use the web app.</b><br><br>The bot doesn\'t have any permissions yet, please create a role where the bot can "Manage webhooks" and assign it to the bot.',
     onPermissionPopupConfirm,
 );
