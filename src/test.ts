c3728d360bf523783954f229076fe652857cf18f
Samuel Alev <samuel.alev@protonmail.com>
2021-06-21 20:02:10 +0200

    chore: remove test file

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import youtubedl from 'youtube-dl-exec';
-
-(async () => {
-    const result = await youtubedl('https://www.twitch.tv/bobross', {
-        getUrl: true,
-    });
-    //@ts-ignore
-    console.log(result as string);
-})();
