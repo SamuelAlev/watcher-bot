e4e8106f74c70dddd0706393b07b9cd0f3b044aa
Samuel Alev <samuel.alev@protonmail.com>
2021-07-05 20:29:57 +0200

    feat: remove new tooltip from Discord

--- src/functions/removeTooltip.ts
+++ src/functions/removeTooltip.ts
@@ -3,12 +3,18 @@ import { Page } from 'puppeteer';
 export default async (page: Page) => {
     const DEBUG = process.env.DEBUG === 'true';
 
-    const tooltip = await page.$('div[class*="channelNotice-"] > div[class*="close-"]');
-    if (!tooltip) {
-        DEBUG && console.log('No tooltip to hide');
-        return;
+    const firstTooltip = await page.$('div[class*="channelNotice-"] > div[class*="close-"]');
+    if (!firstTooltip) {
+        DEBUG && console.log('No first tooltip to hide');
     }
+
+    const secondTooltip = await page.$('#popout_9 button');
+    if (!secondTooltip) {
+        DEBUG && console.log('No second tooltip to hide');
+    }
+
     DEBUG && console.log('Hiding tooltip');
 
-    await tooltip.click();
+    await firstTooltip?.click();
+    await secondTooltip?.click();
 };
